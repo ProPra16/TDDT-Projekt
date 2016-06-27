1d229782d0da233cffd33c60cc5d25e94e1a1040
Mustafa <Mustafa>
2016-06-27 19:00:45 +0200

    Revert "erster commit test"
    This reverts commit da76211b4acc8014256a6e131f1fe3061854d3a0.

--- src/de/hhu/propra16/TDDT/Main.java
+++ src/de/hhu/propra16/TDDT/Main.java
@@ -15,11 +15,9 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-    //test
 
 
     public static void main(String[] args) {
         launch(args);
-        System.out.println("testcommit");
     }
 }
