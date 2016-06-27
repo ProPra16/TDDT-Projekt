e5c75b5a3659af2b3edbd6e217850ef1baf66bb0
Mustafa <Mustafa>
2016-06-27 18:59:55 +0200

    Revert "RED Stage Prototyp"
    This reverts commit 68083a7e5e7e70825fb734f2d5f0752113c87d4a.

--- src/vk/core/internal/InternalCompilerException.java
+++
@@ -1,9 +0,0 @@
-package vk.core.internal;
-
-@SuppressWarnings("serial")
-public class InternalCompilerException extends RuntimeException {
-
-	public InternalCompilerException(String message, Exception cause) {
-		super(message, cause);
-	}
-}
